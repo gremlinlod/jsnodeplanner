d390eac63e56f9aa9939efbad938d62e32442881
Yanis Karklinsh <gremlinlod@gmail.com>
2017-03-29 17:51:30 +0300

    second

--- routes/api.js
+++ routes/api.js
@@ -3,16 +3,21 @@ const router = express.Router();
 
 
 
-router.get('/', (req,res) =>  res.send(`list`));
+router.get('/', (req,res) =>  res.send(`list of todos`));
 
 router.get('/:id', (req,res) =>{
   let id = req.params.id;
-  res.send(`this is ${id}`);
+  res.send(`this is todo with ${id}`);
+});
+
+router.post('/', (req,res)=>{
+  let somedata = {};
+   res.send(`post for new id`);
 });
 
 router.post('/:id', (req,res)=>{
   let id = req.params.id;
-  res.send(`post to ${id}`);
+   res.send(`update existing ${id}`);
 });
 
 module.exports = router;
